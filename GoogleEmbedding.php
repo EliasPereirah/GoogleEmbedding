194d2429b953f9d6d1c65cb826e88cfd2f48e939
EliasPereirah <elias@localhost>
2024-08-04 22:01:56 -0300

    mais de 100 embeddings

--- GoogleEmbedding.php
+++ GoogleEmbedding.php
@@ -8,6 +8,19 @@ class GoogleEmbedding
     **/
     public function embed(array $documents, string $taskType, $model = GOOGLE_EMBEDDING_MODEL)
     {
+        $total_documents = count($documents);
+        $all_embeddings = [];
+        $max = 100;
+        while($total_documents > $max){
+            // Por padrão a API aceita no máximo 100 documentos em batch
+            // Caso seja recebido mais que isso de 100 documentos, será realizado outras mais requisições
+           $doc = array_slice($documents, 0, $max);
+           $new_embeddings = $this->embed($doc, $taskType, $model);
+           $all_embeddings = array_merge($all_embeddings, $new_embeddings);
+           $documents = array_slice($documents, $max);
+           $total_documents = count($documents);
+        }
+
         $google_api_key = GOOGLE_API_KEY;
         $url = "https://generativelanguage.googleapis.com/v1beta/models/$model:batchEmbedContents?key=$google_api_key";
         $req_data = [];
@@ -39,6 +52,14 @@ class GoogleEmbedding
         curl_setopt_array($ch, $options);
         $response = curl_exec($ch);
         curl_close($ch);
-        return json_decode($response);
+        $data = json_decode($response)->embeddings ?? [];
+        if(count($data) === 0){
+            print_r($response);
+        }
+        foreach ($data as $item) {
+            // formato aceito pela ChromaDB
+            $all_embeddings[] = $item->values;
+        }
+        return $all_embeddings;
     }
 }
